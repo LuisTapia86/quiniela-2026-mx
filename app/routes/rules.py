b248769b1fb413ad59376751eb088c13616c2c8e
Nojands <luisjtl86@gmail.com>
2026-04-25 01:00:32 -0600

    add terms and privacy pages

--- app/routes/rules.py
+++ app/routes/rules.py
@@ -6,3 +6,13 @@ bp = Blueprint("rules", __name__, url_prefix="")
 @bp.get("/rules")
 def index():
     return render_template("rules.html")
+
+
+@bp.get("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+@bp.get("/privacy")
+def privacy():
+    return render_template("privacy.html")
