5ac0618e903619c5098f351c417e9db94aa16c29
Nojands <luisjtl86@gmail.com>
2026-04-28 00:22:11 -0600

    chore: remove launch reset controls

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,6 @@ from app.routes.auth import bp as auth_bp
 from app.routes.entries import bp as entries_bp
 from app.routes.leaderboard import bp as leaderboard_bp
 from app.routes.main import bp as main_bp
-from app.routes.emergency_reset import bp as emergency_reset_bp
 from app.routes.rules import bp as rules_bp
 
 __all__ = [
@@ -12,7 +11,6 @@ __all__ = [
     "api_bp",
     "auth_bp",
     "entries_bp",
-    "emergency_reset_bp",
     "leaderboard_bp",
     "main_bp",
     "rules_bp",
